584d0ded3946d5ed25b9094e6cfc72f904497e00
zawette <zawette@gmail.com>
2019-12-13 19:35:42 +0100

    added butterKnife Dependencies

--- app/src/main/java/com/example/jeteste/MainActivity.java
+++ app/src/main/java/com/example/jeteste/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.jeteste;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
